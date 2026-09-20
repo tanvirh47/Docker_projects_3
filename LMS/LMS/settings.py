9bbde87a5e9865e4b681b47c4b1a3165805d8edc
tanvirh <tanvirh934@gmail.com>
2026-09-20 21:59:18 +0600

    Add Dockerized Library Management System

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-cmap$+ara@#wfb=84l9kqsvm*$$wxyq8m385z4p_k@wgtypq=^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
